b86b66a1543aae17dcb8ba00891efd1d675b3fd3
hirokikojima <h.kojima.20140419@gmail.com>
2021-01-10 22:52:33 +0900

    add first typescript component

---
+++ example-app/resources/assets/App.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+const App: FC = () =>  (
+  <div className="container">
+    <h1>Hello laravel + react + typescript.</h1>
+  </div>
+)
+
+export default App
--- example-app/webpack.mix.js
+++ example-app/webpack.mix.js
@@ -11,6 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .react()
+mix.ts('resources/assets/index.tsx', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
